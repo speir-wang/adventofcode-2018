5b7aad27bb21459d20e2834c1dbd8768989b7ebf
He Wang <he@townsquare.agency>
2018-12-04 10:49:48 +1100

    finish day 1

--- day1/script.js
+++ day1/script.js
@@ -1,18 +1,32 @@
 const readInput = require("../readinput");
 const data = readInput("./input.txt").split("\n");
 
-// part 1
+// // part 1
 const result1 = data.reduce((result, currentNumber) => result + parseInt(currentNumber), 0);
 // console.log(result1);
 
 // part 2
-const resultArray = [];
+const temp = [];
+let firstRepeatResult;
+function firstRepeat(loopedArray, initial) {
+	let found = false;
+	let result = initial;
 
-const result2 = data.reduce((result, currentNumber) => {
-	resultArray.push(result);
-	result = result + parseInt(currentNumber);
+	result = loopedArray.slice(0).reduce((result, currentNumber, i, arr) => {
+		if (temp.includes(result)) {
+			found = true;
+			arr.splice(1);
+			firstRepeatResult = result;
+			console.log(result);
+		}
+		temp.push(result);
+		result = result + parseInt(currentNumber);
 
-	return result;
-}, 0);
+		return result;
+	}, result);
+	if (!found) firstRepeat(loopedArray, result);
 
-console.log(resultArray);
+	return firstRepeatResult;
+}
+
+firstRepeat(data, 0);
