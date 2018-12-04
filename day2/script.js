597af69d779750dd7a3321e1667fb0c8eb320911
He Wang <speir.wang@gmail.com>
2018-12-04 23:20:56 +1100

    finish day 2 part 1, replacing the test input with the real input

--- day2/script.js
+++ day2/script.js
@@ -1,28 +1,33 @@
-// const readInput = require("../readinput");
-// const data = readInput("./input.txt").split("\n");
-
-// console.log(data);
-
-// part 1
-
+const readInput = require("../readinput");
+const data = readInput("./input.txt").split("\n");
+/**
+ *
+ * Part 1
+ */
 function findTotalAppearance(boxIDs) {
+	// return [ appearTwice ? 1 : 0, appearThrice ? 1 : 0]
+
 	let appearanceObject = boxIDs.split("").reduce((result, currentLetter, index, array) => {
 		if (!result.hasOwnProperty(currentLetter)) result[currentLetter] = 0;
 
-		if (array.slice(index).includes(currentLetter)) {
-			result[currentLetter] = result[currentLetter] + 1;
-		}
-
+		if (array.slice(index).includes(currentLetter)) result[currentLetter] += 1;
 		return result;
 	}, {});
 
-	console.log(Object.values(appearanceObject));
+	let appearTwice = Object.values(appearanceObject).includes(2) ? 1 : 0;
+	let appearThrice = Object.values(appearanceObject).includes(3) ? 1 : 0;
 
-	return appearanceObject;
+	return [appearTwice, appearThrice];
 }
 
-findTotalAppearance("bababc");
-/**
- * loop through each box ID
- * if letter appears
- * */
+const checksum = data.reduce(
+	(checksumArray, currentBoxID) => {
+		let currentBoxIDResult = findTotalAppearance(currentBoxID);
+		checksumArray[0] += currentBoxIDResult[0];
+		checksumArray[1] += currentBoxIDResult[1];
+
+		return checksumArray;
+	},
+	[0, 0]
+);
+console.log(checksum[0] * checksum[1]);
