1f24d645dc693c90f6fac8d36e6719560583a07d
He Wang <he@townsquare.agency>
2018-12-12 17:38:32 +1100

    finish day 4 part 1

--- day4/script.js
+++ day4/script.js
@@ -1,9 +1,7 @@
 const readInput = require("../readinput");
-const data = readInput("./input.txt")
+const data = require("./input.txt")
 	.trim()
 	.split("\n");
-const { PerformanceObserver, performance } = require("perf_hooks");
-let start = performance.now();
 
 const organizedDateTime = data
 	.map(record => {
@@ -14,41 +12,70 @@ const organizedDateTime = data
 	})
 	.sort((date1, date2) => new Date(date1[0]) - new Date(date2[0]));
 
-/**
- * let a = [ '1518-03-31 00:03', 'Guard #223 begins shift' ]
- *
- */
-/**
- * data structure:
- * {
- *   GuardID: {
- *     minutesOnSleep: number of times sleep
- *   }
- * }
- */
-const finalResult = organizedDateTime.reduce((result, record) => {
-	let id;
-	let time = record[0];
+let currentGuard;
+const sleepingMinutesObject = {};
+for (let i = 0; i < 60; i++) {
+	sleepingMinutesObject[i] = 0;
+}
 
+const organizedList = organizedDateTime.reduce((result, record) => {
 	if (record[1].includes("#")) {
-		id = record[1].split(" ")[1];
-
-		if (!result[id]) {
-			result[id] = Object.create(null);
-			result[id].up = [];
-			result[id].totalSleepTime = 0;
+		currentGuard = record[1].split(" ")[1];
+		if (!result.hasOwnProperty(currentGuard)) {
+			result[currentGuard] = {
+				lastWakeUpTime: new Date(record[0]),
+				totalWakeUpTime: 0,
+				lastSleepTime: "",
+				totalSleepTime: 0,
+				sleepingMinutes: { ...sleepingMinutesObject }
+			};
 		}
-		result[id].up.push(time);
 	}
 
-	if (record[1].includes("up")) {
-		// result[id].up.push(time);
+	if (record[1].includes("falls")) {
+		result[currentGuard].lastSleepTime = new Date(record[0]);
 	}
-	if (record[1].includes("asleep")) {
-		// console.log(JSON.stringify(result));
+
+	if (record[1].includes("wakes")) {
+		result[currentGuard].lastWakeUpTime = new Date(record[0]);
+		// total sleeping time = lastWakeUpTime - lastSleepTime
+		let currentSleepingTime = result[currentGuard].lastWakeUpTime.getMinutes() - result[currentGuard].lastSleepTime.getMinutes();
+
+		result[currentGuard].totalSleepTime = result[currentGuard].totalSleepTime + currentSleepingTime;
+
+		for (let i = result[currentGuard].lastSleepTime.getMinutes(); i < result[currentGuard].lastWakeUpTime.getMinutes(); i++) {
+			result[currentGuard].sleepingMinutes[i] += 1;
+		}
 	}
 
 	return result;
 }, {});
 
-console.log(finalResult);
+// const
+const lazyGuard = Object.entries(organizedList).reduce(
+	(result, guard) => {
+		const guardData = { ...guard[1] };
+		const guardID = guard[0];
+
+		if (guardData.totalSleepTime > result.maxTotalSleepTime) {
+			result.maxTotalSleepTime = guardData.totalSleepTime;
+			result.lazyGuard = guardID;
+			const sortable = [];
+			for (let minute in guardData.sleepingMinutes) {
+				sortable.push([minute, guardData.sleepingMinutes[minute]]);
+			}
+			sortable.sort(function(a, b) {
+				return b[1] - a[1];
+			});
+			const mostSleepMinute = sortable[0];
+			result.mostSleepMinute = {
+				[mostSleepMinute[0]]: mostSleepMinute[1]
+			};
+		}
+
+		return result;
+	},
+	{ lazyGuard: "", maxTotalSleepTime: 0, mostSleepMinute: {} }
+);
+
+console.log(lazyGuard);
