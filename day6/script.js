3235c3a73786ba2b058aa8ffff82160d845fa2b9
He Wang <he@townsquare.agency>
2018-12-13 16:23:47 +1100

    init day 6

---
+++ day6/script.js
@@ -0,0 +1,10 @@
+const readInput = require("../readinput");
+const data = require("./input.txt")
+	.trim()
+	.split("\n");
+
+console.log(data.length);
+
+//  1. find the most far away coordinate
+//  2. name each coordinates
+//  3.
